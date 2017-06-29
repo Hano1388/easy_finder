21568680b8ffe6e61222248170ef18bee071d812
Hano <hinsul3113@gmail.com>
2017-06-29 01:21:55 -0700

    Added store controller, view, routes and fixed store_items migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629075250) do
+ActiveRecord::Schema.define(version: 20170629081611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(version: 20170629075250) do
   end
 
   create_table "store_items", force: :cascade do |t|
-    t.bigint "item_id"
     t.bigint "store_id"
+    t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_store_items_on_item_id"
