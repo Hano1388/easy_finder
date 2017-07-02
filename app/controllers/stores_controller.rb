0b34b7d29d321ad61f2bcc84a8632e99f1334717
Hano <hinsul3113@gmail.com>
2017-07-01 20:14:56 -0700

    change relation between items and shelves and store index and show

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,18 @@
 class StoresController < ApplicationController
+  before_action :find_store, only: [:show]
+
   def index
     @stores = Store.all
   end
+
+  def show
+  end
+
+
+  private
+
+  def find_store
+    @store = Store.find params[:id]
+  end
+
 end
--- db/migrate/20170629074718_add_shelf_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddShelfToItems < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :items, :shelf, foreign_key: true, index: true
-  end
-end
