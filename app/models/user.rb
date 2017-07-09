b968dba5257efd20f20fa2a2e9a7d0e6e1c8f417
Hano <hinsul3113@gmail.com>
2017-07-09 00:01:15 -0700

    Add users and sessions

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, presence: true
+      validates :last_name, presence: true
+
+      VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+      validates :email, presence: true,
+                        uniqueness: {case_sensitive: false},
+                        format: VALID_EMAIL_REGEX
+end
