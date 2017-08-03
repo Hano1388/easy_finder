2e5e1eb35e565f08fcafacc9af1be991461427c2
Hano <hinsul3113@gmail.com>
2017-08-02 17:37:03 -0700

    fix carrierwave issue

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -14,11 +14,13 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
+
+  storage :file
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,4 @@
+# config.fog_provider = 'fog' # 'fog/aws' etc. Defaults to 'fog'
+# config.fog_credentials = {
+#
+# } # Provider specific credentials
